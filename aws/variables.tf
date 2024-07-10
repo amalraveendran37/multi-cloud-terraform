77113247db2e828f646cf9745d44c7140e9a5e69
amalraveendran37 <93665488+amalraveendran37@users.noreply.github.com>
2024-07-10 16:29:18 +0530

    aws

---
+++ aws/variables.tf
@@ -0,0 +1,24 @@
+variable "access" {
+  description = "this is my accese key"
+  default     = "KIAVRUVSAKVYXK3TBPP"
+
+}
+variable "secret" {
+  description = "my key"
+  default     = "5drCWMjlQLLa5sB6qlq52T9nhHeqnti+sArIKFcP"
+
+}
+variable "region" {
+  description = "region"
+  default     = "us-east-1"
+
+}
+variable "instance_type" {
+  description = "instance typye"
+  default     = "t2.micro"
+
+}
+variable "ami_id" {
+  description = "amiid"
+  default     = "ami-06c68f701d8090592"
+}
